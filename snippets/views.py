690d26bf6b21c4d1e3035ca5517bbc78c7486d3a
Rehan Habib <rehan.habib777@gmail.com>
2023-11-27 20:14:37 +0500

    Added Pre-commit

--- snippets/views.py
+++ snippets/views.py
@@ -1,30 +1,23 @@
-from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions, renderers, viewsets
+from rest_framework.decorators import action, api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
-from django.contrib.auth.models import User
-from rest_framework import permissions
+
 from .permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
-from rest_framework import renderers
-from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework import permissions
-
-
-
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     This viewset automatically provides `list` and `retrieve` actions.
     """
+
     queryset = User.objects.all()
-    serializer_class = UserSerializer 
-    
+    serializer_class = UserSerializer
+
 
 class SnippetViewSet(viewsets.ModelViewSet):
     """
@@ -33,10 +26,10 @@ class SnippetViewSet(viewsets.ModelViewSet):
 
     Additionally we also provide an extra `highlight` action.
     """
+
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
     def highlight(self, request, *args, **kwargs):
@@ -44,13 +37,14 @@ class SnippetViewSet(viewsets.ModelViewSet):
         return Response(snippet.highlighted)
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
-    
-@api_view(['GET'])
+        serializer.save(owner=self.request.user)
+
+
+@api_view(["GET"])
 def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })    
-    
-    
+    return Response(
+        {
+            "users": reverse("user-list", request=request, format=format),
+            "snippets": reverse("snippet-list", request=request, format=format),
+        }
+    )
